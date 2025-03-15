8b2dd1cd92466aba6ee60bfc465055033e17eacd
redddeerr <katya8633@gmail.com>
2025-03-15 13:05:13 +0500

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  const apikey = '0bc01f8b0a4c44ffafa63337251503' ;
+  const apiKey = '0bc01f8b0a4c44ffafa63337251503' ;
+
+  const fetchData  = async(cityName) => {
+    const response = await (`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=&{cityName}&aqi=no&lang=ru`)
+    console.log(response);
+  }
+
+  fetchData('Moscow');
+  fetchData('Sochi');
+  fetchData('Perm');
 });
