9613aff8afb2338cc59d5c21906d2a0b13cba379
redddeerr <katya8633@gmail.com>
2025-03-15 13:26:31 +0500

    Update code.js

--- code.js
+++ code.js
@@ -1,20 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const apiKey = '0bc01f8b0a4c44ffafa63337251503' ;
+  const apiKey = 'f2ee48b1843341f083762954250802';
+
+  const searchText = document.getElementById('searchText');
+  const buttonSearch = document.getElementById('buttonSearch');
+  const tableData = document.getElementById('tableData').querySelector('tbody');
 
   const fetchData = async (cityName) => {
     try {
       const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no&lang=ru`);
       const responseJSON = await response.json();
       return responseJSON;
-    } 
-      catch (error) {
+    } catch (error) {
       console.log('error', error);
     }
   }
   
-  fetchData('Moscow');
-  fetchData('Sochi');
-  fetchData('Perm');
+  buttonSearch.addEventListener('click', async () => {
+      const name = searchText.value;
+      if (!name) {
+        alert('Введите название города');
+      } else {
+        const data = await fetchData(name);
+        const tr = document.createElement('tr');
+        const tdName = document.createElement('td');
+        tdName.innerText = name;
+        tr.appendChild(tdName);
+        const tdTemp = document.createElement('td');
+        tdTemp.innerText = data.current.temp_c;
+        tr.appendChild(tdTemp);
+        const tdWeather = document.createElement('td');
+        tdWeather.innerText = data.current.condition.text;
+        tr.appendChild(tdWeather);
+        tableData.appendChild(tr);
+      }
+  })
 });
 
 
