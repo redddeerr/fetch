c4816585a927ab8b041c848d78037ee5983fe502
redddeerr <katya8633@gmail.com>
2025-03-16 15:51:15 +0500

    Update code.js

--- code.js
+++ code.js
@@ -34,6 +34,28 @@ document.addEventListener("DOMContentLoaded", () => {
         tableData.appendChild(tr);
       }
   })
+  searchText.addEventListener('keyup', async (event) => {
+  if (event.key === 'Enter') {
+    const name = searchText.value;
+    if (!name) {
+      alert('Введите название города');
+    } else {
+      const data = await fetchData(name);
+      const tr = document.createElement('tr');
+      const tdName = document.createElement('td');
+      tdName.innerText = name;
+      tr.appendChild(tdName);
+      const tdTemp = document.createElement('td');
+      tdTemp.innerText = data.current.temp_c;
+      tr.appendChild(tdTemp);
+      const tdWeather = document.createElement('td');
+      tdWeather.innerText = data.current.condition.text;
+      tr.appendChild(tdWeather);
+      tableData.appendChild(tr);
+    }
+  }
+});
+  
 });
 
 
